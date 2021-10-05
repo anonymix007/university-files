f7f285446030bf370d00d93c8a18d74c911328d2
anonymix007 <48598263+anonymix007@users.noreply.github.com>
2021-10-05 22:32:38 +0300

    Contest 5 Task 5

--- contests/c5/5/download.sh
+++ contests/c5/5/download.sh
@@ -3,6 +3,6 @@ if [ -f "test8" ]
 then
 echo File was already downloaded and extracted
 else
-curl -L $(yadisk-direct https://disk.yandex.ru/d/VR81CPsFNmHCEA) -o tests.tar.xz
+curl -L $(yadisk-direct https://disk.yandex.ru/d/0rs-mdPFOPSuhw) -o tests.tar.xz
 unxz -v --stdout tests.tar.xz | tar -x
 fi
