a853eaeb3abdeaeb13913413c1bb33c96ccbcb22
anonymix007 <anonymix@ya.ru>
2021-10-30 11:58:25 +0300

    Contest 7 Task 6

--- contests/c7/6/extract.py
+++ contests/c7/6/extract.py
@@ -1,6 +1,11 @@
 import random
-with open('coredump', mode='rb') as file:
-    fileContent = file.read()
+from elftools.elf.elffile import ELFFile
+from elftools.elf.sections import NoteSection
+
+filename = 'coredump'
+file = open(filename, mode='rb')
+fileContent = file.read()
+elffile = ELFFile(file)
 
 print('Searching for 8888 in coredump...');
 for i in range(0, len(fileContent) - 8):
@@ -9,11 +14,17 @@ for i in range(0, len(fileContent) - 8):
     if val == 8888:
         addr = int.from_bytes(fileContent[i + 8 : i + 16],'little')
         print('Found! Next 8 bytes are: ' + hex(addr) + '. This is address of int[], that is, the value in hashmap for key 8888')
+
+
 print('Refer to std::unordered_map definition for more details')
 print('\nThis will print the answer:');
 print('gdb ./with_core coredump')
 print('print *(int*)(' + hex(addr) + ' + sizeof(int))\n')
-print('Or just look at offset ' + hex(4 + addr & 0x16FFF) + ' in coredump')
+
+for segment in elffile.iter_segments():
+    if addr in range(segment['p_vaddr'], segment['p_vaddr'] + segment['p_memsz'] + 1):
+        print('Or look in coredump at offset ' + hex(segment['p_offset'] + addr - segment['p_vaddr'] + 4))
+        break
 
 vvv = random.uniform(0,100)
 if vvv < 10:
