92731303b370549c5d091a58f1d3d3341e3bae35
anonymix007 <anonymix@ya.ru>
2021-12-13 16:19:58 +0300

    Contest 14

---
+++ contests/s3/c14/5/gen2.pl
@@ -0,0 +1,33 @@
+#!/usr/bin/perl
+
+use UUID::Generator::PurePerl;
+
+sub create_search_id {
+    my $this =shift;
+    my $args=shift;
+    my $ug = UUID::Generator::PurePerl->new();
+    my $uuid1 = $ug->generate_v1();
+    return $uuid1;
+}
+
+my ($N_FILES, $N, $CNT_MAX, $INC_MAX, $INIT_MAX) = @ARGV;
+
+open(TEST, '>>', "test13-5_2.txt") or die $!;
+print TEST "CMD/{";
+for my $j (1..$N_FILES) {
+
+
+    my $fuuid = create_search_id();
+    open(FH, '>', "files2/$fuuid.txt") or die $!;
+
+
+    my $value = int(rand($INIT_MAX));
+
+    for my $i (1..$N){
+        my $cnt = int(rand($CNT_MAX));
+        print FH "$value " x $cnt;
+        $value += int(rand($INC_MAX));
+    }
+    print TEST "files2/$fuuid.txt ";
+}
+print TEST "}/\n";
